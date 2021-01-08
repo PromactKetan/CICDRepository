f1ccecf4d6c1624226a3308dcaf06b4cf32e1d04
Ketan <ketan@promactinfo.com>
2021-01-08 10:56:06 +0530

    Initial  Commit

--- CICDpractical/Program.cs
+++ CICDpractical/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace CICDpractical
+namespace CICDPractical
 {
     public class Program
     {
